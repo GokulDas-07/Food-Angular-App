80cff9cce6d2a7727aa33d5788e2dcd42841aa94
Gokul Das M <Gokul@NTP-LAP-723.chn.nestgroup.net>
2022-11-27 22:15:47 +0530

    user login added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 const routes: Routes = [
-  {path:"",component:RegisterComponent}
+  {path:"register",component:RegisterComponent},
+  {path:"",component:UserLoginComponent}
 ];
 
 @NgModule({
